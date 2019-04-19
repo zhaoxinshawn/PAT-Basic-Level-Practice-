9cb6375eff2a88b89782c0ceabf2dccf8db3f9c5
zhaoxinshawn <43698221+zhaoxinshawn@users.noreply.github.com>
2019-04-19 08:23:18 +0800

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+  int n,i;
+  scanf("%d",&n);
+  i=0;
+  while(n!=1){
+    if(n%2==0){
+      n=n/2;
+    }else{
+      n=(3*n+1)/2;
+    }
+    i++;
+  }
+  printf("%d",i);
+  return 0;
+}
